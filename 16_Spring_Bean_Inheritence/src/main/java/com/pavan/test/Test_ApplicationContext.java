56c125c3642f8eb7a3b15ade54cacf9f6ead31dd
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-25 00:42:39 +0530

    Project On Bean Inheritance

---
+++ 16_Spring_Bean_Inheritence/src/main/java/com/pavan/test/Test_ApplicationContext.java
@@ -0,0 +1,40 @@
+package com.pavan.test;
+
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.pavan.beans.CarSetterInjection;
+
+public class Test_ApplicationContext {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		System.out.println("***************Factory Started ******************");
+		
+		System.out.println("Bens Id:: " + Arrays.toString(context.getBeanDefinitionNames())  );
+		
+		
+		CarSetterInjection car1 = context.getBean("car1", CarSetterInjection.class);
+		System.out.println(car1);
+		System.out.println();
+		
+		CarSetterInjection car2 = context.getBean("car2", CarSetterInjection.class);
+		System.out.println(car2);
+		System.out.println();
+		
+		
+		
+		System.out.println("***************Factory Stopped ******************");
+		((AbstractXmlApplicationContext)context).close();
+		
+		
+		
+		
+
+	}
+
+}
