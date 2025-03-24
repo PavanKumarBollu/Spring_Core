c40b75c31d7c651de524560e81fae684ea5b96e9
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-25 01:58:40 +0530

    Project On AutoWiring Concept But Constructor Wiring is not Working properly

--- 20_Spring_AutoWiring_ByName_ByType/src/main/java/com/pavan/test/Test_Application_Context.java
+++ 20_Spring_AutoWiring_ByName_ByType/src/main/java/com/pavan/test/Test_Application_Context.java
@@ -1,5 +1,7 @@
 package com.pavan.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.pavan.beans.Flipkart;
@@ -7,23 +9,29 @@ import com.pavan.beans.Flipkart;
 public class Test_Application_Context {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+		System.out.println("*****CONTAINER STARTED ****\n");
+		System.out.println();
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		
-		System.out.println("*****CONTAINER STARTED****\n");
-		System.out.println();
-		System.out.println();
-
-		Flipkart flipkartBean = factory.getBean("flipkart", Flipkart.class);
-		System.out.println(flipkartBean);
-		System.out.println();
-
-		String result = flipkartBean.doShopping(new String[] { "Lenovo", "ThinkPad" },
-				new Float[] { 1000.0f, 2000.0f });
-
-		System.out.println(result);
-		System.out.println();
-		System.out.println("*****CONTAINER ENDED****\n");
+		System.out.println("AutoWiring By using the The byType Value....");
+		
+		Flipkart flipkart = context.getBean("flipkart1",Flipkart.class);
+		flipkart.doShopping(new String[] {"Mobile","Laptop"}, new Float[] {25000.50f, 100000.0f});
+		
+		System.out.println("AutoWiring By using the The byName Value....");
+		Flipkart flipkart2 = context.getBean("flipkart2",Flipkart.class);
+		flipkart2.doShopping(new String[] {"M","L"}, new Float[] {25000.50f, 100000.0f});
+		
+		
+		System.out.println("AutoWiring By using the The byConstructor Value....");
+		Flipkart flipkart3 = context.getBean("flipkart3",Flipkart.class);
+		flipkart3.doShopping(new String[] {"Watch","Charger"}, new Float[] {25000.50f, 100000.0f});
+		
+		
+		System.out.println("*****CONTAINER STOPPED****\n");
+		((AbstractApplicationContext)context).close();
 	}
 
 }
