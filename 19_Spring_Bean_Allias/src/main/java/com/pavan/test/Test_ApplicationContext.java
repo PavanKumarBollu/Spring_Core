b14aeca8b74c86b270563f50736275ee073d51c3
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-25 01:31:44 +0530

    Project On Allias just like dbms alias

--- 19_Spring_Bean_Allias/src/main/java/com/pavan/test/Test_ApplicationContext.java
+++ 19_Spring_Bean_Allias/src/main/java/com/pavan/test/Test_ApplicationContext.java
@@ -2,30 +2,35 @@ package com.pavan.test;
 
 import java.util.Arrays;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.pavan.beans.DegreeSubjects;
+import com.pavan.messagebean.WishMessageGenarator;
 
 public class Test_ApplicationContext {
 
 	public static void main(String[] args) {
-		
-		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		System.out.println("***************Factory Started ******************");
-		System.out.println("Beans Created:: " + Arrays.toString(context.getBeanDefinitionNames()));
-		
-		DegreeSubjects subBean = context.getBean("degreeSubjects", DegreeSubjects.class);
-		System.out.println(subBean);
-		
-		System.out.println("***************Factory Stopped ******************");
-		
-		
-		
-		
-		
-		context.close();
 
+		ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+		System.out.println("*******Container is starting**********");
+
+		System.out.println("No of beans is :: " + factory.getBeanDefinitionCount());
+		System.out.println("Bean id  is :: " + Arrays.toString(factory.getBeanDefinitionNames()));
+
+		System.out.println();
+
+		WishMessageGenarator wmg = factory.getBean("wmg", WishMessageGenarator.class);
+		System.out.println("HashCode of wmg is :: " + wmg.hashCode());
+		System.out.println(wmg);
+
+		System.out.println();
+
+		WishMessageGenarator wishMessageGenerator = factory.getBean("wishMessageGenarator", WishMessageGenarator.class);
+		System.out.println("HashCode of wishMessageGenerator is :: " + wishMessageGenerator.hashCode());
+		System.out.println(wishMessageGenerator);
+
+		System.out.println("\n*******Container is closing**********");
+		((AbstractApplicationContext) factory).close();
 	}
-
 }
---
+++ 20_Spring_AutoWiring_ByName_ByType/src/main/java/com/pavan/test/Test_Application_Context.java
@@ -0,0 +1,29 @@
+package com.pavan.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.pavan.beans.Flipkart;
+
+public class Test_Application_Context {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		
+		System.out.println("*****CONTAINER STARTED****\n");
+		System.out.println();
+		System.out.println();
+
+		Flipkart flipkartBean = factory.getBean("flipkart", Flipkart.class);
+		System.out.println(flipkartBean);
+		System.out.println();
+
+		String result = flipkartBean.doShopping(new String[] { "Lenovo", "ThinkPad" },
+				new Float[] { 1000.0f, 2000.0f });
+
+		System.out.println(result);
+		System.out.println();
+		System.out.println("*****CONTAINER ENDED****\n");
+	}
+
+}
