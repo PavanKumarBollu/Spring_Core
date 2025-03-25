cbd46ae6cdc56390fb80fd70ffa14161be550601
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-25 23:26:47 +0530

    Project On @AutoWired At Constructor Level

--- 24_Spring_Autowired_ConstructorLevel/src/main/java/com/pavan/beans/Flipkart.java
+++ 24_Spring_Autowired_ConstructorLevel/src/main/java/com/pavan/beans/Flipkart.java
@@ -8,20 +8,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Flipkart {
 
-	@Autowired
-	@Qualifier("fFlight")
 	private ICourier courier = null;
 
 	private ICourier returns = null;
-
-	@Autowired
-	@Qualifier("dtdc")
-	public void setReturns(ICourier returns) {
-		System.out.println("Flipkart.setReturns()");
-		this.returns = returns;
-	}
-
-	private Float discount = null;
 
 	static {
 		System.out.println("flipkart .class file is loaded ..... \n");
@@ -31,24 +20,19 @@ public class Flipkart {
 		System.out.println("flipkart :: zero param Constructor got called");
 
 	}
-
-	public void setCourier(ICourier courier) {
-		System.out.println("Flipkart.setCourier()");
+	@Autowired
+	public Flipkart(@Qualifier("dtdc")ICourier courier, @Qualifier("bDart") ICourier returns) {
+		super();
 		this.courier = courier;
+		this.returns = returns;
 	}
 
-	public void setDiscount(Float discount) {
-		System.out.println("Flipkart.setDiscount()");
-		this.discount = discount;
-	}
+	
 
 	public String doShopping(String[] items, Float[] floats) {
 		System.out.println("Flipkart.doShopping()");
 		System.out.println();
 		System.out.println("Implementation Class is :: " + courier.getClass().getName());
-		System.out.println();
-		System.out.println("discount ::" + discount);
-		System.out.println();
 		System.out.println();
 
 		int orderId = 0;
@@ -59,10 +43,7 @@ public class Flipkart {
 		for (float price : floats) {
 			billAmount += price;
 		}
-
-//		calculating the total based on the discount
-
-		billAmount = billAmount - (discount / 100.0f);
+		billAmount = billAmount - (20 / 100.0f);
 		System.out.println("order Id :: " + orderId);
 
 		String msg = courier.deliver(orderId);
@@ -87,19 +68,17 @@ public class Flipkart {
 			billAmount += price;
 		}
 
-//		calculating the total based on the discount
-
-		billAmount = billAmount - (discount / 100.0f);
+		billAmount = billAmount - (20 / 100.0f);
 		System.out.println("order Id :: " + orderId);
 
 		String msg = returns.deliver(orderId);
 
 		return Arrays.toString(items) + " are Recieved having prices  " + Arrays.toString(floats)
 				+ " with billAmount of " + billAmount + "----->" + msg;
-
 	}
 
-	
+
+
 
 	@Override
 	public String toString() {
