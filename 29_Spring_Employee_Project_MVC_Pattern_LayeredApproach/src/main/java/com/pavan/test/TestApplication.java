9eceee1e40ec98a4d784d2533e732ece343ea817
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-25 07:49:12 +0530

    SpringCore

--- 29_Spring_Employee_Project_MVC_Pattern_LayeredApproach/src/main/java/com/pavan/test/TestApplication.java
+++ 29_Spring_Employee_Project_MVC_Pattern_LayeredApproach/src/main/java/com/pavan/test/TestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import com.pavan.config.AppConfig;
+import com.pavan.config.AppConfig; 
 import com.pavan.controller.MainController;
 import com.pavan.vo.EmployeeVO;
 
